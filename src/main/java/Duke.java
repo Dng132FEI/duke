96542568239c76596799a5c448c028de0180ec1f
unknown <feidong10@hotmail.com>
2019-09-05 21:28:55 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        Scanner in = new Scanner(System.in);
+        String inp;
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        while (true) {
+            inp = in.nextLine();
+            if (inp.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            System.out.println(inp);
+        }
     }
 }
