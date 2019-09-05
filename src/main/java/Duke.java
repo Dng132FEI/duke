4b7d873b0652c58e133ecba5cd99d17f9c76e54b
unknown <feidong10@hotmail.com>
2019-09-06 01:00:58 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,26 +1,60 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
+	
+	public static boolean isNumeric(String str) { 
+        try {  
+		    Integer.parseInt(str);  
+		    return true;
+	    } catch(NumberFormatException e){  
+		    return false;  
+		}  
+	}
+	
+	
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String inp;
-        ArrayList<String> lst = new ArrayList<String>();
+        String inp, inpstrt, check1;
+        Task[] lst = new Task[100];
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
+        int cnt = 0;
         while (true) {
             inp = in.nextLine();
+            if (inp.length() == 6) {
+                inpstrt = inp.substring(0,5);
+                check1 = inp.substring(5,6);
+            } else if (inp.length() == 7) {
+            	inpstrt = inp.substring(0,5);
+                check1 = inp.substring(5,7);
+            } else if (inp.length() == 8) {
+            	inpstrt = inp.substring(0,5);
+                check1 = inp.substring(5,8);
+            } else {
+            	inpstrt = "";
+                check1 = "";
+            }
             if (inp.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } if (inp.equals("list")) {
-            	int len = lst.size();
-            	for (int i = 0; i < len; i++) {
+            } else if (inp.equals("list")) {
+            	System.out.println("Here are the tasks in your list:");
+            	for (int i = 0; i < cnt; i++) {
             		String cur = Integer.toString(i+1);
-            		System.out.println(cur + ". " + lst.get(i));
+            		System.out.println(cur + "." + "[" 
+            		    + lst[i].getStatusIcon() + "] " + lst[i].getDescription());
             	}
+            } else if (inpstrt.equals("done ") && isNumeric(check1)) {
+            	
+            	int idx = Integer.parseInt(check1)-1;
+                lst[idx].setAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[" + lst[idx].getStatusIcon() + 
+                		"] " + lst[idx].getDescription());
             } else {
-            	lst.add(inp);
+            	Task newtsk = new Task(inp);
+            	lst[cnt] = newtsk;
+            	cnt += 1;
                 System.out.println("added: " + inp);
             }
         }
